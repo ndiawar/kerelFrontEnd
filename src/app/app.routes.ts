54a6fba50bca6d3757c26450ad2ec857e3929cc8
okhairy <oumoulkhairyndiaye1.com@gmail.com>
2025-01-31 15:46:23 +0000

    j'ai terminer le front de la liste des utilisateurs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+;
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/User-list', pathMatch: 'full' }, // Redirige vers le tableau de bord par défaut
+  { path: 'User-list', component: UserListComponent },
+];
