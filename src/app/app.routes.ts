f7781caf6d3c7b9d612d883197e5cf23b8f5f108
okhairy <oumoulkhairyndiaye1.com@gmail.com>
2025-01-31 17:23:08 +0000

    j'ai fait le front de l'hitorique des utilisateurs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 ;
 import { UserListComponent } from './components/user-list/user-list.component';
+import { HistoriqueComponent } from './components/historique/historique.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/User-list', pathMatch: 'full' }, // Redirige vers le tableau de bord par défaut
   { path: 'User-list', component: UserListComponent },
+  { path: 'historique', component: HistoriqueComponent },
 ];
--- src/app/components/historique/historique.component.ts
+++ src/app/components/historique/historique.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-historique',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './historique.component.html',
   styleUrl: './historique.component.css'
 })
 export class HistoriqueComponent {
-
+  searchTerm: string = '';
+    actions = [
+      { id: 1, name: 'Abou Sow', description: 'a modifié un nouvel utilisateur', time: '9:00', date: new Date('2023-01-10') },
+      { id: 2, name: 'Ndiaye Diop', description: 'a bloqué un utilisateur', time: '4:59', date: new Date('2023-01-11') },
+      { id: 3, name: 'Oumar Ndiaye', description: 's\'est connecté', time: '1:15', date: new Date('2023-01-12') },
+      { id: 4, name: 'Yaye Kane', description: 'a fermé la vanne', time: '7:45', date: new Date('2023-01-13') },
+      { id: 5, name: 'Roam Research', description: 's\'est connecté', time: '9:32', date: new Date('2023-01-14') },
+      { id: 6, name: 'Ndiaye Diop', description: 's\'est déconnecté', time: '8:15', date: new Date('2023-01-15') },
+      { id: 7, name: 'Oumar Ndiaye', description: 's\'est déconnecté', time: '6:21', date: new Date('2023-01-16') },
+      { id: 8, name: 'Yaye Kane', description: 's\'est connecté', time: '12:51', date: new Date('2023-01-17') },
+    ];
+    get filteredActions() {
+      return this.actions.filter(action =>
+        action.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        action.description.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        this.formatDate(action.date).includes(this.searchTerm)
+      );
+    }
+  
+    formatDate(date: Date): string {
+      return date.toLocaleDateString(); // Format de date pour la comparaison
+    }
 }
